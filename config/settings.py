fe6fc1d383da8800ec2b2e2e2e327a95e9bb0fd3
thisisasadbekuralov <williamsneo12344@gmail.com>
2024-04-24 10:32:12 -0400

    Django filters added

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
     )
 }
 
